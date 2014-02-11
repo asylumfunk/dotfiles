c4c2d43eedb8129c5565b109a40e8da643fddf47
asylumfunk <asylumfunk@users.noreply.github.com>
2014-02-10 22:03:38 -0800

    add installation script
    backup files and directories
    delete symlinks
    
    symlink files with leading period
    exclude .git
    exclude files with multiple periods

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+backup_prefix=backup_
+backup_dir=./${backup_prefix}$(date +%Y%m%d%H%M%S)
+
+mkdir "${backup_dir}"
+
+find \
+	-mindepth 1 -maxdepth 1 \
+	-not -name '.git' -a \
+	-not -name '.*.*' -a \
+	-name '.*' \
+	-exec sh -c '
+		file=${1##*/}
+		src_backup=${HOME}/${file}
+		dst_backup=${2}
+		src_link=${PWD}/${file}
+		dst_link=${HOME}
+
+		if [ -h "${src_backup}" ]; then
+			rm "${src_backup}"
+		elif [ -e "${src_backup}" ]; then
+			mv "${src_backup}" "${dst_backup}"
+		fi
+
+		ln -s "${src_link}" "${dst_link}"
+' -- "{}" "${backup_dir}" \;
+
+find -type d -name "${backup_prefix}*" -empty -delete
+
